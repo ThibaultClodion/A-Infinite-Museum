5e1e437710e0462a124b06aba8c6326ee9cb0505
Thibault Clodion <thibaultclodion@gmail.com>
2026-02-04 08:47:30 +0100

    Add a scriptable object to select artist in game

---
+++ Assets/Code/Artist/ArtistListSO.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ArtistListSO", menuName = "Scriptable Objects/ArtistListSO")]
+public class ArtistListSO : ScriptableObject
+{
+    [SerializeField] private ArtistSO[] _artists;
+    [SerializeField] private List<int> _unusedIndexes;
+
+    public void InitializeIndexes()
+    {
+        // Initialize the indexes
+        _unusedIndexes = new List<int>();
+        for (int i = 0; i < _artists.Length; i++)
+        {
+            _unusedIndexes.Add(i);
+        }
+
+        // Shuffle the indexes
+        int length = _unusedIndexes.Count;
+        for (int i = 0; i < length - 1; i++)
+        {
+            int randomIndex = Random.Range(i, length);
+            int temp = _unusedIndexes[i];
+            _unusedIndexes[i] = _unusedIndexes[randomIndex];
+            _unusedIndexes[randomIndex] = temp;
+        }
+    }
+
+    public ArtistSO GetRandomArtist()
+    {
+        if(_unusedIndexes.Count == 0)
+        {
+            InitializeIndexes();
+        }
+
+        int index = _unusedIndexes[_unusedIndexes.Count - 1];
+        _unusedIndexes.RemoveAt(_unusedIndexes.Count - 1);
+
+        Debug.Log(index);
+
+        return _artists[index];
+    }
+}
